87d18f89736384eef14dbd8ab1f2fc3a02e064ae
Anuska Sampedro <asampedro@vectoritcgroup.com>
2020-06-18 13:30:19 +0200

    Diiferent type binding

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,30 +1,43 @@
-import { Component, OnInit, EventEmitter, Output } from "@angular/core";
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef} from '@angular/core';
 
 @Component({
-  selector: "app-cockpit",
-  templateUrl: "./cockpit.component.html",
-  styleUrls: ["./cockpit.component.css"],
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css'],
 })
 export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output()blueprintCreated = new EventEmitter<{blueprintName: string, blueprintContent: string}>();
-  newServerName = "";
-  newServerContent = "";
-  
+  newServerName = '';
+  // newServerContent = '';
+  @ViewChild('serverContentInput', {static: true}) serverContentInput:ElementRef;
 
   constructor() {}
 
   ngOnInit(): void {}
 
-  onAddServer() {
-  this.serverCreated.emit({
-  serverName: this.newServerName, 
-  serverContent: this.newServerContent})
-  }
-
-  onAddBlueprint() {
-    this.blueprintCreated.emit({
-      blueprintName: this.newServerName, 
-      blueprintContent: this.newServerContent})
-  }
+  // onAddServer() {
+  // this.serverCreated.emit({
+  // serverName: this.newServerName, 
+  // serverContent: this.newServerContent})
+  // }
+  // onAddServer(serverNameInput) {
+  //   console.log(serverNameInput.value);
+  //   this.serverCreated.emit({
+  //   serverName: serverNameInput.value, 
+  //   serverContent: this.serverContentInput})
+  //   }
+  onAddServer(serverNameInput,serverContentInput: HTMLInputElement) {
+    console.log(serverContentInput);
+    this.serverCreated.emit({
+      serverName:serverNameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value
+    })
+   
+    }
+  // onAddBlueprint() {
+  //   this.blueprintCreated.emit({
+  //     blueprintName: this.newServerName, 
+  //     blueprintContent: this.newServerContent})
+  // }
 }
